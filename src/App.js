050fce2310a70e131f3d5821de5676f5f624e356
James Ledger <Jamesledger99@gmail.com>
2023-02-04 22:19:23 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
 
 function Jamesledger(props) {
   const ref = useRef()
-  const { nodes } = useGLTF("/james.gltf")
+  const { nodes } = useGLTF("james.gltf")
   const [hovered, hover] = useState(false)
 
   useFrame((state) => {
